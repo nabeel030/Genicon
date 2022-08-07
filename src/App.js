8d2878013001fbe90884654e504f942acfce1a37
nabeel030 <nabeelahmed030@gmail.com>
2022-08-07 23:42:36 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,6 @@ import Widget from './components/Widget';
 
 function App() {
   return (
-    // <>
-    //   <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    //           <div className="container">
-    //               <a className="navbar-brand" href="#page-top">
-    //                 <img src="assets/img/navbar-logo.svg" alt="..." />
-    //               </a>
-    //               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-    //                   Menu
-    //                   <i className="fas fa-bars ms-1"></i>
-    //               </button>
-    //               <div className="collapse navbar-collapse" id="navbarResponsive">
-    //                   <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-    //                       <li className="nav-item">
-    //                         <a className="nav-link" to="/">Home</a>
-    //                       </li>
-    //                       <li className="nav-item">
-    //                         <a className="nav-link" to="/create-widget">Get Started</a>
-    //                       </li>
-    //                   </ul>
-    //               </div>
-    //           </div>
-    //   </nav>
       <Widget />
   );
 }
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -330,38 +330,31 @@ function Widget() {
                 </div>
             </header>
             {
-                enabledButtonsCount() && (
+                enabledButtonsCount() > 0 && (
                     <>
                         <header className='platform-widgets-container pt-4'>
                             <div className="platform-widgets-subheading">2. Customize button</div>
                             <div className="platform-widgets-heading">
                                 <div className='row justify-content-md-center'>
                                     <div className='col-md-6 platform-widgets'>
-                                        <div className='col-md-12'>
-                                        </div>
                                         <div className='platform-widgets-form text-left'>
                                             {
                                                 enabledButtonsCount() > 1 && (
-                                                    <div className='row'>
-                                                        <div className='col-md-3'>
-                                                            <label class="form-label">Toggle Button Color</label>
+                                                    <div className='row mb-3'>
+                                                        <div className='form-group mt-2'>
+                                                        <label class="form-label">Toggle Button Color</label>
+                                                            <input style={{width: '32.5%'}} type="color" 
+                                                                className="form-control form-control-color"
+                                                                onChange={(e) => setMetaData({...metaData, toggleBtnColor: e.target.value})}
+                                                                value={metaData.toggleBtnColor} 
+                                                                title="Choose your color" />
                                                         </div>
-                                                        <div className='col-md-9'>
-                                                            <div className='form-group mt-2'>
-                                                                <input style={{width: '44%'}} type="color" 
-                                                                    className="form-control form-control-color"
-                                                                    onChange={(e) => setMetaData({...metaData, toggleBtnColor: e.target.value})}
-                                                                    value={metaData.toggleBtnColor} 
-                                                                    title="Choose your color" />
-                                                            </div>
-                                                        </div>
-                                                        <div className='col-md-5'></div>
                                                     </div>
                                                 )
                                             }
                                             Position
                                             <div className='row'>
-                                                <div className='col-md-6'>
+                                                <div className='col-md-6 mb-2'>
                                                     <div className='form-group'>
                                                         <select className='form-control' onChange={(e) => setMetaData({...metaData, position: e.target.value})}>
                                                             <option value='right'>Right</option>
@@ -369,7 +362,7 @@ function Widget() {
                                                         </select>
                                                     </div>
                                                 </div>
-                                                <div className='col-md-6'>
+                                                <div className='col-md-6 mb-2'>
                                                     <div className='form-group'>
                                                         <div className='input-group'>
                                                             <input type='number' className='form-control' min="0" placeholder='Bottom Spacing'
@@ -404,7 +397,7 @@ function Widget() {
                                     {
                                         enableWhatsapp && 
                                         <li>
-                                            <a className="widget-circle widget-whatsapp" href={`tel:${links.whatsapp}`}>
+                                            <a className="widget-circle widget-whatsapp" target='_blank' href={`tel:${links.whatsapp}`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16"><path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/></svg>
                                             </a>
                                         </li>
@@ -412,7 +405,7 @@ function Widget() {
                                     {
                                         enableInsta && 
                                         <li>
-                                            <a className="widget-circle widget-insta" href={`https://www.instagram.com/${links.instagram}`}>
+                                            <a className="widget-circle widget-insta" target='_blank' href={`https://www.instagram.com/${links.instagram}`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16"><path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/></svg>                                        
                                             </a>
                                         </li>
@@ -420,7 +413,7 @@ function Widget() {
                                     {
                                         enableSkype && 
                                         <li>
-                                            <a className="widget-circle widget-skype" href={`${links.skype}`}>
+                                            <a className="widget-circle widget-skype" target='_blank' href={`${links.skype}`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-skype" viewBox="0 0 16 16"><path d="M4.671 0c.88 0 1.733.247 2.468.702a7.423 7.423 0 0 1 6.02 2.118 7.372 7.372 0 0 1 2.167 5.215c0 .344-.024.687-.072 1.026a4.662 4.662 0 0 1 .6 2.281 4.645 4.645 0 0 1-1.37 3.294A4.673 4.673 0 0 1 11.18 16c-.84 0-1.658-.226-2.37-.644a7.423 7.423 0 0 1-6.114-2.107A7.374 7.374 0 0 1 .529 8.035c0-.363.026-.724.08-1.081a4.644 4.644 0 0 1 .76-5.59A4.68 4.68 0 0 1 4.67 0zm.447 7.01c.18.309.43.572.729.769a7.07 7.07 0 0 0 1.257.653c.492.205.873.38 1.145.523.229.112.437.264.615.448.135.142.21.331.21.528a.872.872 0 0 1-.335.723c-.291.196-.64.289-.99.264a2.618 2.618 0 0 1-1.048-.206 11.44 11.44 0 0 1-.532-.253 1.284 1.284 0 0 0-.587-.15.717.717 0 0 0-.501.176.63.63 0 0 0-.195.491.796.796 0 0 0 .148.482 1.2 1.2 0 0 0 .456.354 5.113 5.113 0 0 0 2.212.419 4.554 4.554 0 0 0 1.624-.265 2.296 2.296 0 0 0 1.08-.801c.267-.39.402-.855.386-1.327a2.09 2.09 0 0 0-.279-1.101 2.53 2.53 0 0 0-.772-.792A7.198 7.198 0 0 0 8.486 7.3a1.05 1.05 0 0 0-.145-.058 18.182 18.182 0 0 1-1.013-.447 1.827 1.827 0 0 1-.54-.387.727.727 0 0 1-.2-.508.805.805 0 0 1 .385-.723 1.76 1.76 0 0 1 .968-.247c.26-.003.52.03.772.096.274.079.542.177.802.293.105.049.22.075.336.076a.6.6 0 0 0 .453-.19.69.69 0 0 0 .18-.496.717.717 0 0 0-.17-.476 1.374 1.374 0 0 0-.556-.354 3.69 3.69 0 0 0-.708-.183 5.963 5.963 0 0 0-1.022-.078 4.53 4.53 0 0 0-1.536.258 2.71 2.71 0 0 0-1.174.784 1.91 1.91 0 0 0-.45 1.287c-.01.37.076.736.25 1.063z"/></svg>
                                             </a>
                                         </li>
@@ -428,7 +421,7 @@ function Widget() {
                                     {
                                         enableMsger && 
                                         <li>
-                                            <a className="widget-circle widget-messenger" href={`https://www.facebook.com/${links.messenger}`}>
+                                            <a className="widget-circle widget-messenger" target='_blank' href={`https://www.facebook.com/${links.messenger}`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-messenger" viewBox="0 0 16 16"><path d="M0 7.76C0 3.301 3.493 0 8 0s8 3.301 8 7.76-3.493 7.76-8 7.76c-.81 0-1.586-.107-2.316-.307a.639.639 0 0 0-.427.03l-1.588.702a.64.64 0 0 1-.898-.566l-.044-1.423a.639.639 0 0 0-.215-.456C.956 12.108 0 10.092 0 7.76zm5.546-1.459-2.35 3.728c-.225.358.214.761.551.506l2.525-1.916a.48.48 0 0 1 .578-.002l1.869 1.402a1.2 1.2 0 0 0 1.735-.32l2.35-3.728c.226-.358-.214-.761-.551-.506L9.728 7.381a.48.48 0 0 1-.578.002L7.281 5.98a1.2 1.2 0 0 0-1.735.32z"/></svg>                                        
                                             </a>
                                         </li>
@@ -436,7 +429,7 @@ function Widget() {
                                     {
                                         enableTwitter && 
                                         <li>
-                                            <a className="widget-circle widget-twitter" href={`https://www.twitter.com/${links.twitter}`}>
+                                            <a className="widget-circle widget-twitter" target='_blank' href={`https://www.twitter.com/${links.twitter}`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16"><path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/></svg>
                                             </a>
                                         </li>
@@ -444,7 +437,7 @@ function Widget() {
                                     {
                                         enableCall && 
                                         <li>
-                                            <a className="widget-circle widget-call" href={`tel:${links.call}`}>
+                                            <a className="widget-circle widget-call" target='_blank' href={`tel:${links.call}`}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-telephone" viewBox="0 0 16 16"><path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/></svg>
                                             </a>
                                         </li>
@@ -454,7 +447,7 @@ function Widget() {
                             
                             <ul>
                                 {
-                                    (enableCall + enableInsta + enableMsger + enableSkype + enableTwitter+ enableWhatsapp) > 1 &&
+                                    enabledButtonsCount() > 1 &&
                                     <span style={{background: metaData.toggleBtnColor}} className='widget-circle widget-caret' onClick={() => setShow(!show)}>
                                         {
                                             !show ? 
